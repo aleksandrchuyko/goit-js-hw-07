52d4e517e2812245a16d7996a81431d25685e736
aleksandrchuyko <chao.mess@gmail.com>
2022-05-07 01:27:51 +0300

    Task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,67 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('div.gallery');
+let instance;
+
+function makePictureCards(items) {
+    return items.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+            <a class="gallery__link" href=${original}>
+                <img
+                class="gallery__image"
+                src=${preview}
+                data-source=${original}
+                alt=${description}
+                />
+            </a>
+        </div>`;
+    }).join("");
+}
+
+function onMiniImgClick(e) {
+    let largeImageUrl;
+    if (e.target.parentNode.nodeName === 'A') {
+        e.preventDefault();
+        largeImageUrl = e.target.parentNode.href;
+    }
+    createModal(largeImageUrl).show();
+}
+
+function createModal(largeImageUrl) {
+    instance = basicLightbox.create(`
+    <div class="modal">
+        <img
+            class="gallery__image"
+            src=${largeImageUrl}
+            alt="Image description"
+        />
+    </div>
+`, {
+        onShow: (instance) => {
+            instance.element().addEventListener('click', onModalMouseClick);
+            window.addEventListener('keydown', onModalEscPress);
+        },
+        onClose: (instance) => {
+            instance.element().removeEventListener('click', onModalMouseClick);
+            window.removeEventListener('keydown', onModalEscPress);
+        }
+    });
+    return instance;
+}
+
+const onModalMouseClick = (e) => {
+    instance.close();
+};
+const onModalEscPress = (e) => {
+    if (e.key === 'Escape') {
+        instance.close();
+    }
+};
+
+galleryRef.innerHTML = makePictureCards(galleryItems);
+galleryRef.addEventListener('click', onMiniImgClick);
+
+
+
+
