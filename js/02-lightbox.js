4959497a27950ba61cd3cce724408d4306f3b590
aleksandrchuyko <chao.mess@gmail.com>
2022-05-08 01:31:30 +0300

    Task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('div.gallery');
+let lightbox;
+
+function makePictureCards(items) {
+    return items.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href=${original}>
+                <img class="gallery__image" src=${preview} alt=${description} />
+            </a>`;
+    }).join("");
+}
+
+function onMiniImgClick(e) {
+    if (e.target.parentNode.nodeName === 'A') {
+        e.preventDefault();
+        }
+    
+    lightbox.open(e);
+}
+
+galleryRef.innerHTML = makePictureCards(galleryItems);
+lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: "alt",
+        captionDelay: 250
+});
+    
+galleryRef.addEventListener('click', onMiniImgClick);
+
